70c8bfef92d991a0caea1aa52729fe18fca13481
Zakhar Yakovlev <hezymal@gmail.com>
2017-11-04 06:32:10 +0900

    Include to project Webpack and ReactJS

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 CBen
 ----
+
+-- Use technologies --
+
+- Programming language: JavaScript;
+- Libraries and frameworks: ReactJS;
+- Package builder: Webpack;
+- Package manager: NPM;
+- Version control system: Git;
+- Standarts and formats: HTML, CSS, JSX, JSON.
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx',
+      '.json',
+      '.css'
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              'es2015',
+              'react'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      }
+    ]
+  }
+};
