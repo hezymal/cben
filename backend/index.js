e4530d250cb1808593dad8fa7221d9343da520dd
Zakhar Yakovlev <hezymal@gmail.com>
2017-11-08 15:55:36 +0900

    Add middleware for static files

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.use(express.static('backend/view', {
+  extensions: ['html', 'js', 'css'],
+  index: 'index.html',
+}));
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
